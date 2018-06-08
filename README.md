391a24e98eb0e1e7e0627f310b08b0435a59139b
Dan Albert <danalbert@google.com>
2018-06-08 11:08:47 -0700

    Initial checkin.

---
+++ README.md
@@ -0,0 +1,3 @@
+# GTest JNI Example
+
+Example of exposing native tests as device-side JUnit tests for Studio.
---
+++ app/src/androidTest/java/com/android/gtestjni/ExampleInstrumentedTest.kt
@@ -0,0 +1,7 @@
+package com.android.gtestjni
+
+import org.junit.runner.RunWith
+
+@RunWith(GtestRunner::class)
+@TargetLibrary("apptest")
+class NativeTests
